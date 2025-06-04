7084bde583588ec1a8cfc709fc5da4f1036be76b
hmaya <1556984587@qq.com>
2025-06-04 10:00:56 +0800

    6.4

---
+++ cmake-build-debug/CMakeFiles/ISD2190.elf.dir/DependInfo.cmake
@@ -0,0 +1,92 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/Project/ISD2190/ISD2190/Core/Startup/startup_stm32l151rdtx.s" "E:/Project/ISD2190/ISD2190/cmake-build-debug/CMakeFiles/ISD2190.elf.dir/Core/Startup/startup_stm32l151rdtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L151xD"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/Project/ISD2190/ISD2190/Core/Inc"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Inc"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Inc/Legacy"
+  "E:/Project/ISD2190/ISD2190/Drivers/CMSIS/Device/ST/STM32L1xx/Include"
+  "E:/Project/ISD2190/ISD2190/Drivers/CMSIS/Include"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3"
+  "E:/Project/ISD2190/ISD2190/User"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Project/ISD2190/ISD2190/Core/Src/adc.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/adc.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/dma.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/dma.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/freertos.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/freertos.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/freertos.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/gpio.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/gpio.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/iwdg.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/iwdg.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/iwdg.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/main.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/main.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/stm32l1xx_hal_msp.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_hal_msp.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_hal_msp.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/stm32l1xx_hal_timebase_tim.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_hal_timebase_tim.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/stm32l1xx_it.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_it.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/stm32l1xx_it.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/syscalls.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/sysmem.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/sysmem.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/system_stm32l1xx.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/system_stm32l1xx.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/system_stm32l1xx.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/tim.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/tim.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Core/Src/usart.c" "CMakeFiles/ISD2190.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Core/Src/usart.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc_ex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_adc_ex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_cortex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_cortex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_dma.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_dma.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_dma.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_exti.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_exti.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_exti.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ramfunc.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_flash_ramfunc.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_gpio.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_gpio.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_iwdg.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_iwdg.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_iwdg.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr_ex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_pwr_ex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc_ex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_rcc_ex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim_ex.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_tim_ex.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_uart.c" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_uart.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Drivers/STM32L1xx_HAL_Driver/Src/stm32l1xx_hal_uart.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/CollectData.c" "CMakeFiles/ISD2190.elf.dir/User/CollectData.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/CollectData.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/VMxx.c" "CMakeFiles/ISD2190.elf.dir/User/VMxx.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/VMxx.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/bt4531.c" "CMakeFiles/ISD2190.elf.dir/User/bt4531.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/bt4531.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/flash.c" "CMakeFiles/ISD2190.elf.dir/User/flash.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/flash.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/hardware.c" "CMakeFiles/ISD2190.elf.dir/User/hardware.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/hardware.c.obj.d"
+  "E:/Project/ISD2190/ISD2190/User/temp.c" "CMakeFiles/ISD2190.elf.dir/User/temp.c.obj" "gcc" "CMakeFiles/ISD2190.elf.dir/User/temp.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ISD2190.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ISD2190.elf.
+# This may be replaced when dependencies are built.
